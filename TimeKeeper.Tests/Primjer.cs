a291e0fa4d8be3d6bf660901c6b6843facf889b3
shtef21 <stjepan.salopek@hotmail.com>
2023-03-13 00:00:46 +0100

    UT - UserShould and PrimjerClass

---
+++ TimeKeeper.Tests/Primjer.cs
@@ -0,0 +1,39 @@
+﻿namespace TimeKeeper.Tests;
+
+// Imenovanje - KlasaShould
+// Klasa - klasa čije se metode testiraju
+public class KlasaShould
+{
+
+    // [Fact] - test koji se vrši jednom
+    [Fact]
+    public void DoSomething()
+    {
+        // Klasa should do something
+
+        // SUT - System Under Test
+        int sut1 = 1;
+        int sut2 = 2;
+
+        Assert.NotEqual(sut1, sut2);
+    }
+
+    // [Theory] - više testova
+    [Theory]
+    // [InlineData(BR)] - ulazne vrijednosti za testove
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public void Multiply(int value)
+    {
+        // Klasa should multiply
+        // (U pravilu bi se pozivala metoda koja pripada klasi Klasa)
+
+        // int mult = Klasa.Multiply(value, 2)
+        int mult = value * 2;
+
+        int expected = value * 2;
+        Assert.Equal(mult, expected);
+    }
+
+}
--- TimeKeeper.Tests/UserShould.cs
+++ TimeKeeper.Tests/UserShould.cs
@@ -1,31 +1,63 @@
+using System.Text.RegularExpressions;
 using TimeKeeper.Models;
 
-namespace TimeKeeper.Tests {
+namespace TimeKeeper.Tests; 
 
-    public class UserShould { //test klasa se naziva prema onoj klasi koja se testira, dodaje se should koji u kombinaciji s nazivom testa oznacava sto klasa treba raditi -> User Should Generate Random Password
+public class UserShould {
 
-        [Fact] //oznaka za test, vidi xunit dokumentaciju za bolje objsanjenje i razliku izmedu [Fact] i [Theory] anotacija
-        public void GenerateRandomPassword() { //funkcija koja je zapravo test
+    [Fact]
+    public void GenerateRandomPassword()
+    {
+        // User should generate random password
 
-            User sut1 = new User("firstName", "lastName", "email@mail.com", false, 7.33, 1); //sut -> System Under Test, oznaka za ono sto se testira
-            User sut2 = new User("firstName", "lastName", "email@mail.com", false, 7.33, 1);
+        User sut1 = new User("firstName", "lastName", "email@mail.com", false, 7.33, 1);
+        User sut2 = new User("firstName", "lastName", "email@mail.com", false, 7.33, 1);
 
-            Assert.NotEqual(sut1.password, sut2.password); //Assert je klasa koja izvršava testiranje i vraca true ili false ovisno o rezultatu pozvane metode. Ovdje se usporeduje da su 2 lozinke razlièite s metodom NotEqual u sluèaju da su lozinke iste, test ce failat
-
-            /*
-                Testovi se u Visual Studiu pokrecu kroz test explorer, mozes ga naci na vrhu ekrana pod Test
-                Solution se mora buildati kad se doda novi test, nekad i kad se rade promjene na postojeæem testu
-                Test možes debugirati tako da u test exploreru kliknes desni klik na njega i odaberes debug
-                Ovdje imas popis nekih (nezz jesu sve) Assert metodi:
-                    https://gist.github.com/jonesandy/f622874e78d9d9f356896c4ac63c0ac3
-                    https://textbooks.cs.ksu.edu/cis400/1-object-orientation/04-testing/05-xunit-assertions/
-                Ideje za testove:
-                    provjeriti da li user generira validan guid pomoæu regexa
-                    provjeriti da je duljina generirane lozinke 8 znakova
-             */
-
-
-
-        }
+        Assert.NotEqual(sut1.password, sut2.password);
     }
+
+    [Fact]
+    public void GenerateProperGuid()
+    {
+        User sut1 = new User("firstName", "lastname", "example@email.com", false, 1.0, 1);
+
+        // GUID pattern - XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+        // Where X is a hex digit (0123456789ABCDEF)
+        Regex guidRegex = new Regex(
+            @"[0-9A-F]{8}-([0-9A-F]{4}-){3}[0-9A-F]{12}",
+            RegexOptions.IgnoreCase
+        );
+
+        Assert.Matches(guidRegex, sut1.guid);
+    }
+
+    [Fact]
+    public void InitializeProperly()
+    {
+        // Check if all parameters are set properly
+        int id = 3;
+        string firstName = "John";
+        string lastName = "Doe";
+        string email = "john.doe@example.com";
+        string password = "some_pw";
+        bool isAdmin = true;
+        double payPerHour = 13.0;
+        int companyId = 1234;
+        double grade = 5.0;
+        string guid = System.Guid.NewGuid().ToString();
+
+        var sut = new User(id, firstName, lastName, email, password, isAdmin, payPerHour, companyId, grade, guid);
+
+        Assert.Equal(sut.id, id);
+        Assert.Equal(sut.firstName, firstName);
+        Assert.Equal(sut.lastName, lastName);
+        Assert.Equal(sut.email, email);
+        Assert.Equal(sut.password, password);
+        Assert.Equal(sut.isAdmin, isAdmin);
+        Assert.Equal(sut.payPerHour, payPerHour, 0.00001);
+        Assert.Equal(sut.companyId, companyId);
+        Assert.Equal(sut.grade, grade, 0.001);
+        Assert.Equal(sut.guid, guid);
+    }
+
 }
